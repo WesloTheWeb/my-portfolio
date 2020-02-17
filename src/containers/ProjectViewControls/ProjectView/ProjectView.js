639782c46635f45217e0aeecad74d4de7deb2e36
Wesley Webster <wesleywebster91@gmail.com>
2020-02-17 17:14:40 -0600

    Projects transferred
    System in place

--- src/containers/ProjectViewControls/ProjectView/ProjectView.js
+++ src/containers/ProjectViewControls/ProjectView/ProjectView.js
@@ -5,9 +5,14 @@ import Project from './Project/Project';
 // IMAGES
 import Kohls from '../../../assets/images/Kohls_Sample.png';
 import kohlsSample from '../../../assets/files/WorkSample_Kohls.pdf';
+import medicalCollegeWisconsin from '../../../assets/images/MCW_Sample.png';
+import MCW_Sample from '../../../assets/files/CaseStudy-MCW.pdf';
+import mainStreetJusticeSample from '../../../assets/images/MSJ_Sample.png';
+import MSJ_Sample from '../../../assets/files/CaseStudy-MSJ.pdf';
+
 
 // CSS
-const { projectViewGrid, } = classes;
+const { projectViewGrid, locked } = classes;
 
 
 const ProjectView = (props) => {
@@ -21,11 +26,41 @@ const ProjectView = (props) => {
                     <img src={Kohls} alt="Kohl's spotlight preview" />
                 </a>
             </Project>
-            <Project />
-            <Project />
-            <Project />
-            <Project />
-            <Project />
+            <Project>
+                <a href={MCW_Sample} target="_blank" rel="noopener noreferrer">
+                    <img src={medicalCollegeWisconsin} alt="Medical College of Wisconsin spotlight preview" />
+                </a>
+            </Project>
+            <Project>
+                <a href={MSJ_Sample} target="_blank" rel="noopener noreferrer">
+                    <img src={mainStreetJusticeSample} alt="Main Street Justice's spotlight preview" />
+                </a>
+            </Project>
+            <Project>
+                <a href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={locked}>
+                    Coming Soon!
+                </a>
+            </Project>
+            <Project>
+                <a href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={locked}>
+                    Coming Soon!
+                </a>
+            </Project>
+            <Project>
+                <a
+                    href="/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={locked}>
+                    Coming Soon!
+                </a>
+            </Project>
         </section>
     );
 };
