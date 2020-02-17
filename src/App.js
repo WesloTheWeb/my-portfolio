bf026fdad7aef87d4d418a598b5cdbf32fa315df
Wesley Webster <wesleywebster91@gmail.com>
2020-02-17 17:49:24 -0600

    Maintenance

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navigation from './components/Navigation/Navigation';
 import SectionIntroduction from './components/SectionIntroduction/SectionIntroduction';
 import Bio from './components/Bio/Bio';
 import Social from './components/Social/Social';
-import ProjectViewControls from './containers/ProjectViewControls/ProjectViewControls';
+// import ProjectViewControls from './containers/ProjectViewControls/ProjectViewControls';
 import ProjectView from './containers/ProjectViewControls/ProjectView/ProjectView';
 import Experience from './containers/Experience/Experience';
 import Contact from './components/Contact/Contact';
@@ -19,16 +19,16 @@ function App() {
           <Bio />
           <Social />
         </div>
-        <section className="contentSection">
+        <section id="projects" className="contentSection">
           <SectionIntroduction>Projects</SectionIntroduction>
-          <ProjectViewControls />
+          {/* <ProjectViewControls /> */}
           <ProjectView />
         </section>
-        <section className="contentSection">
+        <section id="experience" className="contentSection">
           <SectionIntroduction>Experience</SectionIntroduction>
           <Experience />
         </section>
-        <section className="contentSection">
+        <section id="contact" className="contentSection">
           <SectionIntroduction>Contact</SectionIntroduction>
           <Contact />
         </section>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Navigation.module.css';
 import Auxillary from '../../hoc/Auxillary/Auxillary';
+import resume from '../../assets/files/WesleyWebster_Resume.pdf';
 
-const { nav, navContainer } = classes
+const { nav, navContainer } = classes;
+
+function Scroll () {
+    const [prevScrollPos, newScrollPos] = useState(0);
+
+}
 
 const navigation = (props) => {
+
+ 
     return (
         <Auxillary>
         <div className={navContainer}>
             <a href="/" className={nav}>Home</a>
-            <a href="/" className={nav}>Projects</a>
-            <a href="/" className={nav}>Experience</a>
-            <a href="/" className={nav}>Resume</a>
-            <a href="/" className={nav}>Contact</a>
-
+            <a href="#projects" className={nav}>Projects</a>
+            <a href="#experience" className={nav}>Experience</a>
+            <a href={resume} target="_blank" rel="noopener noreferrer" className={nav}>Resume</a>
+            <a href="#contact" className={nav}>Contact</a>
         </div>
         </Auxillary>
      );
